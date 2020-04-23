ad46b9ca084e6fe8d66f704a22b8dc5bf6ae3694
korejan <64199710+korejan@users.noreply.github.com>
2020-04-23 11:35:51 +0100

    Add files via upload
    Initial port & upload, (currently not working).

---
+++ rigid_transform_3D.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <Eigen/Geometry>
+
+//
+// Ported/Adapted from: https://github.com/nghiaho12/rigid_transform_3D/blob/master/rigid_transform_3D.py
+// Based on article: http://nghiaho.com/?page_id=671
+//
+using PointSet = Eigen::Matrix<float, 3, Eigen::Dynamic>;
+auto rigid_transform_3D(const PointSet& A, const PointSet& B)
+{
+	static_assert(PointSet::RowsAtCompileTime == 3);
+	assert(A.cols() == B.cols());
+
+	// find mean column wise
+	const Eigen::Vector3f centroid_A = A.rowwise().mean();
+	const Eigen::Vector3f centroid_B = B.rowwise().mean();
+
+	// subtract mean
+	PointSet Am = A.colwise() - centroid_A;
+	PointSet Bm = B.colwise() - centroid_B;
+
+	PointSet H = Am * Bm.transpose();
+
+	//
+	//# sanity check
+	//#if linalg.matrix_rank(H) < 3:
+	//	#    raise ValueError("rank of H = {}, expecting 3".format(linalg.matrix_rank(H)))
+	//
+
+	// find rotation
+	auto svd = H.jacobiSvd(Eigen::DecompositionOptions::ComputeFullU | Eigen::DecompositionOptions::ComputeFullV);
+	const auto& U = svd.matrixU();
+	auto Vt = svd.matrixV();
+	Eigen::Matrix3f R = Vt.transpose() * U.transpose();
+
+	//	# special reflection case
+	if (R.determinant() < 0.0f)
+	{
+		Vt.row(2) *= -1.0f;
+		R = Vt.transpose() * U.transpose();
+	}
+
+	auto t = -R * centroid_A + centroid_B;
+
+	return std::make_tuple(R, Eigen::Vector3f(t));
+}
+
+int main()
+{
+	// Test with random data
+
+	// Random rotationand translation
+	Eigen::Matrix3f R = Eigen::Matrix3f::Random();
+	Eigen::Vector3f t = Eigen::Vector3f::Random();
+
+	// make R a proper rotation matrix, force orthonormal
+	auto svd = R.jacobiSvd(Eigen::DecompositionOptions::ComputeFullU | Eigen::DecompositionOptions::ComputeFullV);
+	const auto& U = svd.matrixU();
+	auto Vt = svd.matrixV();
+	R = U * Vt;
+	
+	// remove reflection
+	if (R.determinant() < 0.0f)
+	{
+		Vt.row(2) *= -1.0f;
+		R = U * Vt;
+	}
+
+	// number of points
+	constexpr const std::size_t N = 10;
+
+	PointSet A = PointSet::Random(3, N);
+	PointSet B = (R * A).colwise() + t;
+
+	// Recover R and t
+	auto [ret_R, ret_t] = rigid_transform_3D(A, B);
+
+	// Compare the recovered R and t with the original
+	PointSet B2 = (ret_R * A).colwise() + ret_t;
+
+	// Find the root mean squared error
+	PointSet err = B2 - B;
+	err = err.cwiseProduct(err);
+	float rmse = std::sqrt(err.sum() / static_cast<float>(N));
+
+	std::cout << "Points A\n" << A << '\n';
+
+	std::cout << "Points B\n" << B << '\n';
+
+	std::cout << "Ground truth rotation\n" << R << '\n';
+
+	std::cout << "Recovered rotation\n" << ret_R << '\n';
+
+	std::cout << "Ground truth translation\n" << t << '\n';
+
+	std::cout << "Recovered translation\n" << ret_t << '\n';
+
+	std::cout << "RMSE: " << rmse << '\n';
+
+	if (rmse < 1e-5f)
+		std::cout << "Everything looks good!\n";
+	else
+		std::cout << "Hmm something doesn't look right ...\n";
+}
